e6f2e0f90d50c8296573cbf014e55907da88a845
Mariano Jerez <marianojerez1984@gmail.com>
2023-03-30 17:14:18 -0300

    Test Calculadora

--- EjercicioGit/src/ejerciciogit/EjercicioGit.java
+++ EjercicioGit/src/ejerciciogit/EjercicioGit.java
@@ -9,13 +9,33 @@ package ejerciciogit;
  * @author Onna
  */
 public class EjercicioGit {
+static class Calculadora{
 
+        public Calculadora() {
+        }
+        public float sumar(float num1, float num2){
+            return num1+num2;
+        }
+        public float restar(float num1, float num2){
+            return num1-num2;
+        }
+        public float dividir(float num1, float num2){
+            return num1/num2;
+        }
+        public float multiplicar(float num1, float num2){
+            return num1*num2;
+        }
+}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo!");
+        Calculadora calc = new Calculadora();
+        System.out.println("El resultado de multiplicar 80 por 3 es : "+calc.multiplicar(80, 3) );
+        System.out.println("El resultado de sumar 180 más 150, y dividirlo por 3 es : "+calc.dividir(calc.sumar(150, 180),3) );
+        System.out.println("El resultado de restar 90 y 50, y multiplicarlo por 15 es : "+calc.multiplicar(calc.restar(90, 50), 15) );
+        System.out.println("El resultado de sumar 70 y 40 y multiplicarlo por 25 es : "+calc.multiplicar(calc.sumar(70,40), 25) );
     }
     
 }
